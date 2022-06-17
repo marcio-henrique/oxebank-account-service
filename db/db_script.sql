cc1a4774cd8fd5c0d137a5a822bcfd39ad53a51a
Márcio Henrique <marcioh.ikke@gmail.com>
2022-06-16 21:29:23 -0300

    add db script

---
+++ db/db_script.sql
@@ -0,0 +1,35 @@
+create database oxebank_account_service;
+use oxebank_account_service;
+
+CREATE table `users` (
+`id` bigint PRIMARY KEY AUTO_INCREMENT,
+`type` int,
+`email` varchar(80),
+`status` int,
+`address` varchar(255),
+`password` varchar(80),
+`phone` string(50),
+`api_token` varchar(80)
+);
+
+CREATE TABLE `clients` (
+`id` int PRIMARY KEY AUTO_INCREMENT,
+`user_id` bigint,
+`type` int,
+`mounthly_income` int,
+`official_document` int
+);
+
+CREATE TABLE `banking_accounts` (
+`id` int PRIMARY KEY AUTO_INCREMENT,
+`client_id` int,
+`type` int,
+`status` int,
+`balance` int,
+`password` varchar(80),
+`api_token` varchar(80)
+);
+
+ALTER TABLE `clients` ADD FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+
+ALTER TABLE `banking_accounts` ADD FOREIGN KEY (`client_id`) REFERENCES `clients` (`id`);
